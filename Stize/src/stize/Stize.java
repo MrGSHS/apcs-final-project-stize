585a34defb09b986d93de76777f99e87af421333
KatiaVille <evillev6@students.d125.org>
2016-05-03 14:24:52 -0500

    stize project made

---
+++ Stize/src/stize/Stize.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package stize;
+
+/**
+ *
+ * @author evillev6
+ */
+public class Stize {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
