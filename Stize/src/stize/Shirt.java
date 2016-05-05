0edff069e04e23fbc578ad21b24551fd93ad8ede
KatiaVille <evillev6@students.d125.org>
2016-05-05 14:13:09 -0500

    Added Class files

---
+++ Stize/src/stize/Shirt.java
@@ -0,0 +1,14 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package stize;
+
+/**
+ *
+ * @author evillev6
+ */
+public class Shirt extends Clothes{
+    
+}
