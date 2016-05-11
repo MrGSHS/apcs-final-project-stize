7a364acc587e6145eecc83f8b44b5119d4607b9c
KatiaVille <evillev6@students.d125.org>
2016-05-11 14:32:17 -0500

    started intiliaze inventory

--- Stize/src/stize/InitializeInventory.java
+++ Stize/src/stize/InitializeInventory.java
@@ -5,11 +5,40 @@
  */
 package stize;
 
+import java.util.ArrayList;
+
 /**
  *
  * @author katiadance
  */
 public class InitializeInventory {
     //use this class to create a static inventory object and add to it the different types of clothing
+
+    //for loop to create each type of object
     
+    private ArrayList<Dress> dresses;
+    private ArrayList<Pants> pants;
+    private ArrayList<Shirt> shirts;
+    private ArrayList<Skirt> skirts;
+    
+    public InitializeInventory(){};
+    
+    public ArrayList<Dress> getDresses(){
+    Dress dress1 = new Dress((int)Math.random()*3+1,(int)Math.random()*10+10,"H&M", "blue");
+    Dress dress2 = new Dress((int)Math.random()*3+1,(int)Math.random()*10+10,"Sears", "blue");
+    Dress dress3 = new Dress((int)Math.random()*3+1,(int)Math.random()*100+100,"Burberry", "blue");
+    Dress dress4 = new Dress((int)Math.random()*3+1,(int)Math.random()*40+10,"American Eagle", "blue");
+    Dress dress5 = new Dress((int)Math.random()*3+1,(int)Math.random()*10+10,"Forever 21", "blue");
+    Dress dress6 = new Dress((int)Math.random()*3+1,(int)Math.random()*10+10,"H&M", "blue");
+    Dress dress7 = new Dress((int)Math.random()*3+1,(int)Math.random()*10+10,"H&M", "blue");
+    Dress dress8 = new Dress((int)Math.random()*3+1,(int)Math.random()*20+10,"Sears", "blue");
+    Dress dress9 = new Dress((int)Math.random()*3+1,(int)Math.random()*10+10,"Forever 21", "blue");
+    Dress dress10 = new Dress((int)Math.random()*3+1,(int)Math.random()*40+10,"American Eagle", "blue");
+    dresses.add(dress1);
+    
+    }
+    
+    
+    }
+  
 }
